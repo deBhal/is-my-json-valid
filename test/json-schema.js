5d1cbd0c82e8d9e5ea3a1f05684ed1e43d8819d0
Julian de Bhal <julian.debhal@automattic.com>
2017-12-23 12:00:11 +1000

    Refactor/parameterise json-schema.js
    This doesn't change the logic. The test output before and after is identical:
    $ diff before after && echo OK
    OK

--- test/json-schema.js
+++ test/json-schema.js
@@ -2,22 +2,23 @@ var tape = require('tape')
 var fs = require('fs')
 var validator = require('../')
 
-var files = fs.readdirSync(__dirname+'/json-schema-draft4')
-  .map(function(file) {
-    if (file === 'definitions.json') return null
-    if (file === 'refRemote.json') return null
-    return require('./json-schema-draft4/'+file)
-  })
-  .filter(Boolean)
+var readDir = function(dir, {excluding} = {excluding: []}) {
+  return fs.readdirSync(__dirname+'/'+dir)
+    .filter(file => ! (Array.isArray(excluding) && excluding.includes(file)))
+    .map(file => './'+dir+'/'+file)
+}
 
-files.forEach(function(file) {
-  file.forEach(function(f) {
-    tape('json-schema-test-suite '+f.description, function(t) {
-      var validate = validator(f.schema)
-      f.tests.forEach(function(test) {
-        t.same(validate(test.data), test.valid, test.description)
+var loadAndRunTests = suiteName =>
+  fileName => require(fileName)
+    .forEach(f =>
+      tape(suiteName+' '+f.description, t => {
+        var validate = validator(f.schema)
+        f.tests.forEach(function(test) {
+          t.same(validate(test.data), test.valid, test.description)
+        })
+        t.end()
       })
-      t.end()
-    })
-  })
-})
+    )
+
+readDir('json-schema-draft4', {excluding: ['definitions.json', 'refRemote.json']})
+  .forEach(loadAndRunTests('json-schema-test-suite'))
